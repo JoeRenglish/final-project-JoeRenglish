790a49f272e5a6becd4edee2b2f7ffe6c984647d
Joseph English <work@Josephs-MacBook-Pro.local>
2024-06-05 01:00:24 -0700

    made some progress

---
+++ CatJack/src/js/card.js
@@ -0,0 +1,60 @@
+class Card {
+
+    //Initialize private local variables
+    #value
+    #suit
+    #img
+
+    //Create constructor with two parameters
+    constructor(value, suit, img) {
+        this.#value = value;
+        this.#suit = suit;
+        this.#img = img;
+    }
+
+    //Getters and setters for value
+    get salue() { return this.#value; }
+    //Validate input for setter
+    set value(value) {
+        if (value >= 1 && value <= 13) {
+            this.#value = value;
+        } else {
+            throw new Error("Value must be between 1 and 13");
+        }
+    }
+
+    //Getters and setters for suit
+    get suit() { return this.#suit; }
+    //Validate input for setter
+    set suit(suit) {
+        if (suit >= 1 && suit <= 4) {
+            this.#suit = suit;
+        } else {
+            throw new Error("Suit must be between 1 and 4");
+        }
+    }
+
+    //Getters and setter for img
+    get img() { return this.#img; }
+    set img(src) { return src;}
+
+    //Get if the card is an Ace
+    get isAce() {
+        if (value == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Get if the card is a face card
+    get isFaceCard() {
+        if (value >= 11 && value <= 13) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
---
+++ CatJack/src/js/deck.js
@@ -0,0 +1,44 @@
+class Deck {
+    
+    constructor() {
+        this.cards = [];
+        let i = 0;
+        for (let value = 1; value <= 13; value++) {
+            for (let suit = 1; suit <= 4; suit++) {
+                i++;
+                this.cards.push(new Card(value, suit, `${i}.png`))
+            }
+        }
+    }
+
+    get deckSize() { return this.cards.length }
+    get isEmpty() { if (this.deckSize == 0) return true; }
+
+
+    random(min, max) {  
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    suffleDeck() {
+        for (let i = 0; i < this.deckSize; i++) {
+            let j = this.random(0, this.deckSize);
+            let temp = this.cards[i];
+            this.cards[i] = this.cards[j];
+            this.cards[j] = this.cards[temp];
+        }
+    }
+
+    deal() {
+
+        if (!this.isEmpty) {
+            // get a refernce to the first card
+            const c = this.cards[0];
+            // remove the card from the list
+            this.cards.shift();
+            // return the first card
+            return c;
+        }
+    }
+}
+
+
